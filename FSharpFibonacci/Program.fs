b5d5d75b1ecc6e1f2b2f9dcd91cfeb764873e596
Arthur <arthrp@users.noreply.github.com>
2020-08-19 21:10:17 +0300

    Initial commit

---
+++ FSharpFibonacci/Program.fs
@@ -0,0 +1,16 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+let rec fibonnacci n =
+    match n with
+    | 0 -> 0
+    | 1 -> 1
+    | _ -> fibonnacci (n - 1) + fibonnacci (n - 2)
+
+[<EntryPoint>]
+let main argv =
+    let num = int argv.[0]
+    let x = fibonnacci num
+    printfn "Result: %d" x
+    0 // return an integer exit code
